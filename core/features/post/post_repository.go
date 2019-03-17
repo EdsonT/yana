4fb204e8551753412f94087433aea2754ee9ecbd
Cosme1234 <44682891+Cosme1234@users.noreply.github.com>
2019-03-17 17:49:36 -0400

    modified:   core/features/post/post_repository.go

--- core/features/post/post_repository.go
+++ core/features/post/post_repository.go
@@ -79,3 +79,19 @@ func CountRecords() int64 {
 	fmt.Println(res)
 	return res
 }
+func DeletCompanyPhysics(cod string) *mongo.SingleResult {
+	Init()
+	res := coll.FindOneAndDelete(context.TODO(), bson.D{{"code", cod}})
+	return res
+}
+func DeletCompanyLogical(cod string) (*mongo.UpdateResult, error) {
+	Init()
+	params := bson.D{{"status", "Inactive"}}
+	filter := bson.D{{"code", cod}}
+	update := bson.D{
+		{"$set", params},
+	}
+	res, err := coll.UpdateOne(context.TODO(), filter, update)
+	fmt.Println(res, err)
+	return res, err
+}
