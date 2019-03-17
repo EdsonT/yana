55b94587cce84f33394b2c09090c4d5607e709c3
Cosme1234 <44682891+Cosme1234@users.noreply.github.com>
2019-03-17 17:50:54 -0400

    modified:   model/post.go

--- model/post.go
+++ model/post.go
@@ -10,6 +10,7 @@ type Post struct {
 	Company     string    `form:"company,omitempty" json:"company,omitempty" bson:"company,omitempty"`
 	Location    string    `form:"location,omitempty" json:"location,omitempty" bson:"location,omitempty"`
 	Type        string    `form:"type,omitempty" json:"type,omitempty" bson:"type,omitempty"`
+	Status      string    `form:"status,omitempty" json:"status,omitempty" bson:"status,omitempty"`
 	Description string    `form:"description,omitempty" json:"description,omitempty" bson:"description,omitempty"`
 	DateCreated time.Time `form:"dateCreated,omitempty" json:"dateCreated,omitempty" bson:"dateCreated,omitempty"`
 	LastUpdated time.Time `form:"lastUpdated,omitempty" json:"lastUpdated,omitempty" bson:"lastUpdated,omitempty"`
