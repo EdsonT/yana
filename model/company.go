8908076fe970aa3c0ab2a0b723a7fbbdc23ee690
Cosme1234 <44682891+Cosme1234@users.noreply.github.com>
2019-03-17 14:54:50 -0400

    modified:   model/company.go

--- model/company.go
+++ model/company.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Company struct {
-	Code        string
-	Name        string
-	City        string
-	Country     string
-	Address     string
-	Description string
-	DateCreated time.Time
-	LastUpdated time.Time
+	Code        string    `form:"code,omitempty" json:"code,omitempty" bson:"code,omitempty"`
+	Name        string    `form:"name,omitempty" json:"name,omitempty" bson:"name,omitempty"`
+	City        string    `form:"city,omitempty" json:"city,omitempty" bson:"city,omitempty"`
+	Country     string    `form:"country,omitempty" json:"country,omitempty" bson:"country,omitempty"`
+	Address     string    `form:"address,omitempty" json:"address,omitempty" bson:"address,omitempty"`
+	Description string    `form:"description,omitempty" json:"description,omitempty" bson:"description,omitempty"`
+	DateCreated time.Time `form:"dateCreated,omitempty" json:"dateCreated,omitempty" bson:"dateCreated,omitempty"`
+	LastUpdated time.Time `form:"lastUpdated,omitempty" json:"lastUpdated,omitempty" bson:"lastUpdated,omitempty"`
 }
