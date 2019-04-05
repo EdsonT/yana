b04c566e1e60929763d868ba7a881756e5825cf7
Edson Tarqui <ednoosde.wob@gmail.com>
2019-04-05 01:13:57 -0400

    modified:   src/components/AddPost.vue

--- web/muyok/src/components/AddPost.vue
+++ web/muyok/src/components/AddPost.vue
@@ -1,28 +1,47 @@
 <template>
-  <v-container grid-list-xl>
+  <v-container grid-list-md>
     <v-layout wrap justify-space-between>
       <v-flex xs12>
         <v-form ref="form">
-          <v-text-field v-model="model" label="Post Title:"></v-text-field>
-          <v-textarea outline label="Description:"></v-textarea>
+          <v-text-field id="title" label="Post Title:" v-model="title"></v-text-field>
+          <v-textarea id="description" outline label="Description:" v-model="description"></v-textarea>
         </v-form>
       </v-flex>
-
-      <v-flex xs12 md8>
-        <v-slider v-model="max" label="Max characters"></v-slider>
-
-        <v-checkbox v-model="allowSpaces" label="Allow spaces"></v-checkbox>
-
-        <v-text-field v-model="match" label="Value must match"></v-text-field>
+      <v-flex xs12>
+        <v-text-field id="company" label="Company" v-model="company"></v-text-field>
       </v-flex>
-      <v-flex xs12 md4>
-        <v-text-field v-model="model" :counter="max" :rules="rules" label="First name"></v-text-field>
+      <v-flex xs6>
+        <v-text-field id="location" label="Location" v-model="location"></v-text-field>
+      </v-flex>
+      <v-flex xs6>
+        <v-select id="type" :items="items" label="Type" v-model="type"></v-select>
+      </v-flex>
+      <v-flex>
+        <v-btn color="success" @click="onSubmit">Create</v-btn>
+      </v-flex>
+      <v-flex>
+        <v-btn color="warning">Cancel</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
-export default {};
+export default {
+  data: () => ({
+    items: ["Remote", "Local"],
+    title: "",
+    description: "",
+    company: "",
+    location: "",
+    type: ""
+  }),
+  methods: {
+    onSubmit(){
+      alert('submitting')
+    }
+  },
+};
+
 </script>
 <style lang="">
 </style>
